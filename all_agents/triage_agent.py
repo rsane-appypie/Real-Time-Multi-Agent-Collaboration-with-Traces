56c6481e33765105da102552b8388b152d0026f1
Ravi Sane <rsane@appypiellp.com>
2025-04-10 05:21:09 +0000

    Homework agent created with traces

--- all_agents/triage_agent.py
+++ all_agents/triage_agent.py
@@ -2,12 +2,17 @@ from agents import Agent, InputGuardrail
 from .history_tutor_agent import history_tutor_agent
 from .math_tutor_agent import math_tutor_agent
 from .guardrail import homework_guardrail
+import mlflow
+mlflow.openai.autolog()
+# Optional: Set a tracking URI and an experiment
+mlflow.set_tracking_uri("http://localhost:8080")
+mlflow.set_experiment("OpenAI")
 
 triage_agent = Agent(
     name="Triage Agent",
     instructions="You determine which agent to use based on the user's homework question",
     handoffs=[history_tutor_agent, math_tutor_agent],
-    # input_guardrails=[
-    #     InputGuardrail(guardrail_function=homework_guardrail),
-    # ],
+    input_guardrails=[
+        InputGuardrail(guardrail_function=homework_guardrail),
+    ],
 )
